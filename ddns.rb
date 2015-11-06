e06c99d3b3f32eccdb46ee94b1d97a7c89392d44
Jirapong Nanta <jirapong.nanta@gmail.com>
2015-11-06 10:49:30 +0700

    a simple record import for dynamic dns

---
+++ ddns.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+
+token="DOMAIN_TOKEN"
+domain_id="DOMAIN_ID"
+record_id="RECORD_ID"
+
+jsonip = open('http://jsonip.com').read
+if jsonip =~ /(\d+\.\d+\.\d+\.\d+)/
+  cmd = "curl -H \"Accept: application/json\""
+  cmd << " -H \"Content-Type: application/json\""
+  cmd << " -H \"X-DNSimple-Domain-Token: #{token}\""
+  cmd << " -X \"PUT\""
+  cmd << " -i \"https://api.dnsimple.com/v1/domains/#{domain_id}/records/#{record_id}\""
+  cmd << " -d \"{\\\"record\\\":{\\\"content\\\":\\\"#{my_ip}\\\"}}\""
+  system(cmd)
+end
